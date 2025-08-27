5b1b02ed3bd5aa6520d4d0ccccdda8de40c24022
Marcelo Nicolas Ramallo De Dios <nicooavenged@gmail.com>
2025-08-27 12:05:13 -0300

    Add new functionality to change state of the task with a new Enum

--- Workspace/Project1/main.cpp
+++ Workspace/Project1/main.cpp
@@ -3,28 +3,35 @@
 #include <string>
 using namespace std;    
 
-void ChooseAction(map<string , map<int, bool>>);
-void ShowAllTasks(map<string , map<int, bool>>); 
-map<string , map<int, bool>> SearchOrCreate(map<string , map<int, bool>>, string);
+enum ECompletionState{
+    CREATED,
+    STARTED,
+    COMPLETED
+    };
+
+void ChooseAction(map<string , map<int, ECompletionState>>);
+void ShowAllTasks(map<string , map<int, ECompletionState>>); 
+map<string , map<int, ECompletionState>> SearchOrCreate(map<string , map<int, ECompletionState>>, string);
 
 int main ()
 {
     
-    map<int, bool> taskInfo;
+    map<int, ECompletionState> taskInfo;
     
-    map<string , map<int, bool>> task;
+    map<string , map<int, ECompletionState>> task;
     
     ChooseAction(task);
-
+    
     return 0;    
 }
 
-void ChooseAction(map<string , map<int, bool>> task)
+void ChooseAction(map<string , map<int, ECompletionState>> task)
 {
     cout << "Choose how to continue" << endl;
     cout << "1: Search or Create Task" << endl;
     cout << "2: Show All Tasks" << endl;
-    cout << "3: Shut down program" << endl;
+    cout << "3: Change Completion State" << endl;
+    cout << "4: Shut Down Program" << endl;
 
     int keyIn;
     cin >> keyIn;
@@ -45,18 +52,52 @@ void ChooseAction(map<string , map<int, bool>> task)
             ChooseAction(task);
             break;}
         case 3:{
+            cout << "Write which task to do you want to search information" << endl;
+            string taskName;
+            cin >> taskName;
+    
+            auto taskSelected = SearchOrCreate(task, taskName);
+            
+            cout << "Choose Completion State" << endl;
+            cout << "1: Created" << endl;
+            cout << "2: Started" << endl;
+            cout << "3: Completed" << endl;
+
+            int keyIn;
+            cin >> keyIn;
+            
+            ECompletionState completionChoosed;
+            switch(keyIn){
+            case 1:{
+                completionChoosed = ECompletionState::CREATED;
+                break;}
+            case 2:{
+                completionChoosed = ECompletionState::STARTED;
+                break;}
+            case 3:{
+                completionChoosed = ECompletionState::COMPLETED;
+                break;}}
+            
+            for (auto& [taskId, completionStatus]: taskSelected [taskName])
+            {
+                taskSelected[taskName] = {{taskId, completionChoosed}};           
+            };
+            
+            ChooseAction(taskSelected);
+            break;}
+        case 4:{
         break;}};
         
 };
 
-map<string , map<int, bool>> SearchOrCreate(map<string , map<int, bool>> taskIn, string keyIn)
+map<string , map<int, ECompletionState>> SearchOrCreate(map<string , map<int, ECompletionState>> taskIn, string taskName)
 {
     
-    if(taskIn.contains(keyIn) == 1)
+    if(taskIn.contains(taskName) == 1)
     {
-        for (auto& [taskId, bIsComplete]: taskIn [keyIn])
+        for (auto& [taskId, completionStatus]: taskIn [taskName])
         {
-            cout << "numero de tarea es: " << taskId << "\n" << "La tarea esta completada: " << bIsComplete << "\n";
+            cout << "numero de tarea es: " << taskId << "\n" << "La tarea esta completada: " << completionStatus << "\n";
         }
         return taskIn;
     }
@@ -64,8 +105,8 @@ map<string , map<int, bool>> SearchOrCreate(map<string , map<int, bool>> taskIn,
     {
         cout << "El mapa no contiene tu tarea, te la crearemos ahora" << endl;
         
-        taskIn[keyIn].insert({ taskIn.size(), false});
-        SearchOrCreate(taskIn, keyIn);
+        taskIn[taskName].insert({ taskIn.size(), ECompletionState::CREATED});
+        SearchOrCreate(taskIn, taskName);
         cout << "\n";
         cout << "\n";
         
@@ -74,15 +115,15 @@ map<string , map<int, bool>> SearchOrCreate(map<string , map<int, bool>> taskIn,
         
 }
 
-void ShowAllTasks(map<string , map<int, bool>> taskIn)
+void ShowAllTasks(map<string , map<int, ECompletionState>> taskIn)
 {
     for (auto& [task, taskInfo]: taskIn)
     {
         cout << "Nombre de tarea " << task << "\n";
 
-        for (auto& [taskId, bIsComplete]: taskInfo)
+        for (auto& [taskId, completionStatus]: taskInfo)
         {
-            cout << "El numero de tarea es: " << taskId << "\n" << "La tarea esta completada: "<< bIsComplete << "\n";
+            cout << "El numero de tarea es: " << taskId << "\n" << "La tarea esta completada: "<< completionStatus << "\n";
             cout << "\n";
             cout << "\n";
         }
